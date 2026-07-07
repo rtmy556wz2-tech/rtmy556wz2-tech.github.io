02bd2a3ffd727643aa284ebe44e85d722483f4e0
user100299 <mateuszkonieczy@gmail.com>
2026-07-08 00:34:25 +0200

    Create German.js

---
+++ vocabulary/German.js
@@ -0,0 +1,12 @@
+window.GermanVocabulary = [
+  { word: "Mond", meaning: "moon" },
+  { word: "Wald", meaning: "forest" },
+  { word: "mutig", meaning: "brave" },
+  { word: "Stern", meaning: "star" },
+  { word: "Freund", meaning: "friend" },
+  { word: "Baum", meaning: "tree" },
+  { word: "Fluss", meaning: "river" },
+  { word: "Blume", meaning: "flower" },
+  { word: "Vogel", meaning: "bird" },
+  { word: "Himmel", meaning: "sky" }
+];
